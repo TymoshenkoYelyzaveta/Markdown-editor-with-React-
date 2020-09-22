7ec3ab5dc99aa8f71e67278ec2fbb7264d9c254d
Liza <tymoshenkoyelyzaveta.s@gmail.com>
2020-09-22 13:26:36 +0200

    first functionality

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component, useState } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+      markdown: '# sup',
+    };
+  }
+
+  handleChange = (e) => {
+    this.setState({ markdown: e.target.value });
+  };
+
+  render() {
+    return (
+      <div className='app'>
+        <textarea
+          onChange={this.handleChange}
+          value={this.state.markdown}
+          className='markdown-editor'
+        />
+        <div className='markdown-preview'>{this.state.markdown}</div>
+      </div>
+    );
+  }
 }
 
 export default App;
